e53e0a664a0025292bb5e846aa64c3b2613d1061
Stéphane (Bakeneko) Dupont <aleistor@gmail.com>
2021-04-23 11:56:38 +0200

    feat: add color utils and extensions

---
+++ src/AmongUsRevamped/Extensions/ColorExtensions.cs
@@ -0,0 +1,43 @@
+﻿using AmongUsRevamped.Utils;
+using UnityEngine;
+
+namespace AmongUsRevamped.Extensions
+{
+    public static class ColorExtensions
+    {
+        public static Color32 ToColor32(this Color color)
+        {
+            return ColorUtils.ToColor32(color);
+        }
+
+        public static string ToHtmlStringRGB(this Color color)
+        {
+            return ColorUtils.ToHtmlStringRGB(color);
+        }
+
+        public static string ToHtmlStringRGB(this Color32 color)
+        {
+            return ColorUtils.ToHtmlStringRGB(color);
+        }
+
+        public static string ToHtmlStringRGBA(this Color color)
+        {
+            return ColorUtils.ToHtmlStringRGBA(color);
+        }
+
+        public static string ToHtmlStringRGBA(this Color32 color)
+        {
+            return ColorUtils.ToHtmlStringRGBA(color);
+        }
+
+        public static string ToColorTag(this Color color)
+        {
+            return $"<color={ColorUtils.ToHtmlStringRGBA(color)}>";
+        }
+
+        public static string ToColorTag(this Color32 color)
+        {
+            return $"<color={ColorUtils.ToHtmlStringRGBA(color)}>";
+        }
+    }
+}
---
+++ src/AmongUsRevamped/Utils/ColorUtils.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace AmongUsRevamped.Utils
+{
+    public static class ColorUtils
+    {
+
+        public static Color32 ToColor32(Color color)
+        {
+            // Round to int to prevent precision issues that, for example cause values very close to 1 to become FE instead of FF (case 770904).
+            return new Color32(
+                (byte)Mathf.Clamp(Mathf.RoundToInt(color.r * 255), 0, 255),
+                (byte)Mathf.Clamp(Mathf.RoundToInt(color.g * 255), 0, 255),
+                (byte)Mathf.Clamp(Mathf.RoundToInt(color.b * 255), 0, 255),
+                1);
+        }
+
+        public static string ToHtmlStringRGB(Color color)
+        {
+            return ToHtmlStringRGB(ToColor32(color));
+        }
+
+        public static string ToHtmlStringRGB(Color32 color)
+        {
+            return $"#{color.r:X2}{color.g:X2}{color.b:X2}";
+        }
+
+        public static string ToHtmlStringRGBA(Color color)
+        {
+            return ToHtmlStringRGBA(ToColor32(color));
+        }
+
+        public static string ToHtmlStringRGBA(Color32 color)
+        {
+            return $"#{color.r:X2}{color.g:X2}{color.b:X2}{color.a:X2}";
+        }
+    }
+}
