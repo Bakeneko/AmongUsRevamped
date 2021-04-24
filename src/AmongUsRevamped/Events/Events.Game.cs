6f8daac8c59dbb00e60b3ab37093e66f225c0b90
Stéphane (Bakeneko) Dupont <aleistor@gmail.com>
2021-04-24 10:46:27 +0200

    feat: add vote events

--- src/AmongUsRevamped/Events/Events.Game.cs
+++ src/AmongUsRevamped/Events/Events.Game.cs
@@ -1,19 +1,46 @@
-using System;
+﻿using System;
+using System.Linq;
 using AmongUsRevamped.Extensions;
 using AmongUsRevamped.Utils;
 using HarmonyLib;
+using UnhollowerBaseLib;
 
 namespace AmongUsRevamped.Events
 {
     [HarmonyPatch]
     public static partial class GameEvents
     {
+        public static event EventHandler<VoteCastedEventArgs> VoteCasted;
+        public static event EventHandler<VotingCompletedEventArgs> VotingCompleted;
         public static event EventHandler<VentEventArgs> VentEntered;
         public static event EventHandler<VentEventArgs> VentExited;
 
+        [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.CastVote))]
+        [HarmonyPrefix]
+        private static void CastVote(
+            MeetingHud __instance,
+            [HarmonyArgument(0)] byte votingPlayerId,
+            [HarmonyArgument(1)] sbyte votedPlayerId)
+        {
+            var player = GameData.Instance.AllPlayers.ToArray().FirstOrDefault(pi => pi.PlayerId == votingPlayerId);
+            var voted = GameData.Instance.AllPlayers.ToArray().FirstOrDefault(pi => pi.PlayerId == votedPlayerId);
+            VoteCasted?.SafeInvoke(__instance, new VoteCastedEventArgs(player, voted), nameof(VoteCasted));
+        }
+
+        [HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.VotingComplete))]
+        [HarmonyPostfix]
+        private static void VotingComplete(
+            MeetingHud __instance,
+            [HarmonyArgument(0)] Il2CppStructArray<byte> states,
+            [HarmonyArgument(1)] GameData.PlayerInfo ejectedPlayer,
+            [HarmonyArgument(2)] bool EMBDDLIPBME)
+        {
+            VotingCompleted?.SafeInvoke(__instance, new VotingCompletedEventArgs(ejectedPlayer), nameof(VotingCompleted));
+        }
+
         [HarmonyPatch(typeof(Vent), nameof(Vent.EnterVent), typeof(PlayerControl))]
         [HarmonyPrefix]
-        public static void EnterVent(Vent __instance, [HarmonyArgument(0)] PlayerControl player)
+        private static void EnterVent(Vent __instance, [HarmonyArgument(0)] PlayerControl player)
         {
             var system = ShipUtils.GetSystem(player.GetTruePosition());
             VentEntered?.SafeInvoke(__instance, new VentEventArgs(system, player), nameof(VentEntered));
@@ -21,7 +48,7 @@ namespace AmongUsRevamped.Events
 
         [HarmonyPatch(typeof(Vent), nameof(Vent.ExitVent), typeof(PlayerControl))]
         [HarmonyPrefix]
-        public static void ExitVent(Vent __instance, [HarmonyArgument(0)] PlayerControl player)
+        private static void ExitVent(Vent __instance, [HarmonyArgument(0)] PlayerControl player)
         {
             var system = ShipUtils.GetSystem(player.GetTruePosition());
             VentExited?.SafeInvoke(__instance, new VentEventArgs(system, player), nameof(VentExited));
--- src/AmongUsRevamped/Events/Events.GameEventArgs.cs
+++ src/AmongUsRevamped/Events/Events.GameEventArgs.cs
@@ -4,6 +4,28 @@ namespace AmongUsRevamped.Events
 {
     public static partial class GameEvents
     {
+        public class VoteCastedEventArgs : EventArgs
+        {
+            public readonly GameData.PlayerInfo Player;
+            public readonly GameData.PlayerInfo VotedPlayer;
+
+            public VoteCastedEventArgs(GameData.PlayerInfo player, GameData.PlayerInfo votedPlayer)
+            {
+                Player = player;
+                VotedPlayer = votedPlayer;
+            }
+        }
+
+        public class VotingCompletedEventArgs : EventArgs
+        {
+            public readonly GameData.PlayerInfo EjectedPlayer;
+
+            public VotingCompletedEventArgs(GameData.PlayerInfo ejectedPlayer)
+            {
+                EjectedPlayer = ejectedPlayer;
+            }
+        }
+
         public class VentEventArgs : EventArgs
         {
             public int Id;
