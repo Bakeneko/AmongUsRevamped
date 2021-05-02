735e90faa82fc2facb840d51d31bd8cb45dc6554
Stéphane (Bakeneko) Dupont <aleistor@gmail.com>
2021-05-02 11:35:20 +0200

    feat: patch ban points

---
+++ src/AmongUsRevamped/Mod/StatsManager.Patch.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+
+namespace AmongUsRevamped.Mod
+{
+    [HarmonyPatch]
+    public static class StatsManagerPatch
+    {
+        /// <summary>
+        /// Deactivate bans
+        /// </summary>
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(StatsManager), nameof(StatsManager.BanPoints), MethodType.Getter)]
+        public static void BanPointsGetterPatch(ref float __result)
+        {
+            __result = 0;
+        }
+
+        /// <summary>
+        /// Deactivate bans
+        /// </summary>
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(StatsManager), nameof(StatsManager.BanPoints), MethodType.Setter)]
+        public static void BanPointsSetterPatch([HarmonyArgument(0)]ref float points)
+        {
+            points = 0;
+        }
+
+        /// <summary>
+        /// Deactivate bans
+        /// </summary>
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(StatsManager), nameof(StatsManager.AmBanned), MethodType.Getter)]
+        public static void AmBannedPatch(ref bool __result)
+        {
+            __result = false;
+        }
+    }
+}
